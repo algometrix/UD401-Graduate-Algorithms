d82b72d2dd51d000492333c5edb5ed65b3343a66
Ashish Nagar <ashishnagar31@gmail.com>
2018-08-14 02:20:01 +0530

    Removed global variable and changes in print statement

--- Dynamic Programming 1/lis.py
+++ Dynamic Programming 1/lis.py
@@ -1,28 +1,25 @@
 # Code to find the length of the longest increasing sequence
 
-length_longest = []
-
 def lis(sn):
-    global length_longest
+    length_longest = []
+    for i in range(len(sequence_of_numbers)):
+        length_longest.append(1)
     for i in range(0, len(sn)):
-        print(" I : {}".format(i))
         for j in range(0, i ):
-            print(" J : {}".format(j))
-            print('sn[{}] : {} | sn[{}] : {} | ll[{}] : {} | ll[{}]:{}'.format(j,sn[j],i,sn[i],j,length_longest[j],i,length_longest[i] ))
+            print('sn[{}] : {} | sn[{}] : {} | ll[{}] : {} | ll[{}]:{}'.format(i,sn[i],j,sn[j],i,length_longest[i],j,length_longest[j] ))
             if sn[j] < sn[i] and length_longest[j] >= length_longest[i]:
                 length_longest[i] = 1 + length_longest[j]
 
+    return length_longest
+    
+
 def max_lis_length(sequence_of_numbers):
-    lis(sequence_of_numbers)
+    return max(lis(sequence_of_numbers))
 
 def main(sequence_of_numbers):
-    max_lis_length(sequence_of_numbers)
+    print('Max length : {}'.format(max_lis_length(sequence_of_numbers))) 
 
 if __name__ == '__main__':
-    sequence_of_numbers = [ 1,0,3,6,2 ]
-    for i in range(len(sequence_of_numbers)):
-        length_longest.append(1)
-    
+    sequence_of_numbers = [ 1,2,3,4,5,6,0,3,4,5,6, ]
     print(sequence_of_numbers)
     main(sequence_of_numbers)
-    print(length_longest)
